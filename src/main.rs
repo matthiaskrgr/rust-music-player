723dc094d0a54094875a3506fe59bc144ccdb6d8
Matthias Krüger <matthias.krueger@famsik.de>
2017-10-14 00:30:31 +0200

    we can now play all the oggs we find in repo root

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,36 @@ extern crate rodio; // https://github.com/tomaka/rodio/
 extern crate ncurses; // https://github.com/jeaye/ncurses-rs
 
 
+use std::fs;
 use std::fs::File;
 use std::io::BufReader;
+//use std::path::Path;
+use std::ffi::OsStr;
 
 use ncurses::*;
 
 
 fn main() {
+
+    let mut playable_files =  Vec::new() ;
+    // iterate over current directory
+    if let Ok(files) = fs::read_dir("./") {
+        for filename in files {
+            if let Ok(filename) = filename {
+                // filter playable files here
+
+                if  filename.path().extension() == Some(OsStr::new("ogg")) {
+                    println!("Found file: {}", filename.path().display());
+                    // collect filenames
+                    playable_files.push(filename);
+                }
+
+            }
+        }
+    }
+
+
+
     // init ncurses
     initscr();
     raw();
@@ -41,10 +64,11 @@ fn main() {
     let endpoint = rodio::get_default_endpoint().unwrap();
     let sink = rodio::Sink::new(&endpoint);
 
-    let file = File::open("test.ogg").unwrap();
-    let audio_source = rodio::Decoder::new(BufReader::new(file)).unwrap();
-
-    sink.append(audio_source);
+    for filename in playable_files {
+        let file = File::open(filename.path()).unwrap();
+        let audio_source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+        sink.append(audio_source);
+    }
     sink.sleep_until_end(); // play everything until end
 
     println!("done");
