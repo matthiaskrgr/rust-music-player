1243c29494ee2a6c5e59e7a7d139f41b7bb758f0
Matthias Krüger <matthias.krueger@famsik.de>
2017-10-16 23:28:41 +0200

    fix bugs and improve things

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,8 @@ extern crate ncurses; // https://github.com/jeaye/ncurses-rs
 use std::fs;
 use std::fs::File;
 use std::io::BufReader;
-//use std::path::Path;
 use std::ffi::OsStr;
-//use std::{time, thread};
 use ncurses::*;
-
 
 struct PathWithString {
     dir_entry: std::fs::DirEntry,
@@ -20,6 +17,7 @@ struct PathWithString {
 }
 
 fn main() {
+
     let mut playable_files = Vec::new();
     // iterate over current directory
     if let Ok(files) = fs::read_dir("./") {
@@ -28,7 +26,6 @@ fn main() {
 
                 // filter playable files here
                 if filename.path().extension() == Some(OsStr::new("ogg")) {
-                    //println!("Found file: {}", filename.path().display());
                     // collect filenames
                     let path = filename.path().display().to_string();
                     let path_w_string = PathWithString {
@@ -42,51 +39,38 @@ fn main() {
         } // for filename in files
     } // all files in cwd
 
-/*    for i in &playable_files {
-        println!("{}", i.path_string);
-    } */
-
     // init ncurses
     initscr();
     raw();
-    // invisible cursor
-    //    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE); // invisible cursor
 
     // new window
-    let w = newwin(10, 12, 1, 1);
-    box_(w, 0, 0);
-    let i = 0;
-/*    for file in &playable_files {
-        // highlight
-        if i == 3 {
-            wattron(w, A_STANDOUT as u32);
-        } else {
-            wattroff(w, A_STANDOUT as u32);
+    let window_x = playable_files.len() as i32;
+    let mut window_y = 0;
+    // find out longest song name
+    for songname in &playable_files {
+        let length = songname.path_string.len() as i32;
+        if length > window_y {
+            window_y = length;
         }
-        mvprintw(i + 1, 2, "menu entry\n");
-        wrefresh(w);
-    } // for
-    wrefresh(w);
-    */
+    }
 
 
-    printw("Testing ncurses screen, e to exit\n\n");
+    // pub fn newwin(lines: i32, cols: i32, y: i32, x: i32) -> WINDOW
+    let w = newwin(window_x + 4, window_y + 5, 1, 0); // 25: get max song length
+    box_(w, 0, 0);
+
+    printw("Rust Music Player. E to exit.");
 
     for file in &playable_files {
         addstr(&format!("{}\n", file.path_string));
     }
-
-    mvprintw(LINES() - 1, 0, "e to exit");
     refresh();
-
-
-
-    //thread::sleep(time::Duration::from_secs(4));
+    noecho(); // dont echo keys
 
     let mut max_x = 0;
     let mut max_y = 0;
     getmaxyx(stdscr(), &mut max_y, &mut max_x);
-
     addstr(&format!(
         "window resolutions: \t max_x: {}, max_y: {}",
         max_x,
@@ -97,9 +81,7 @@ fn main() {
     let mut i = 0 as i32;
     let mut play = "";
     loop {
-
-        let  ch = getch();
-        //print!("{}\n", ch as i32);
+        let ch = getch();
         if ch == 'e' as i32 {
             // terminate
             endwin();
@@ -122,45 +104,37 @@ fn main() {
         } else if ch == 'p' as i32 {
             // play
             play = &playable_files[i as usize].path_string;
-            endwin();
-            break;
+        //endwin();
+        //break;
         } else {
             // nope
         }
         wrefresh(w);
+        let index = i;
+        // use default_endpoint() once this works
+        let endpoint = rodio::get_default_endpoint().unwrap();
+        let sink = rodio::Sink::new(&endpoint);
+
+        let filename = &playable_files[index as usize].path_string;
+        let file = File::open(filename).unwrap();
+        let audio_source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+        sink.append(audio_source);
+        sink.sleep_until_end(); // play everything in queu
     }
-    println!("playing  {}", play);
-    let index = i;
-    println!("playing audio");
-    let endpoint = rodio::get_default_endpoint().unwrap(); // use default_endpoint() once this works
-    let sink = rodio::Sink::new(&endpoint);
-
-    let pathrev = &playable_files[index as usize];
-    let path_string = &pathrev.path_string;
-    let pathref = &pathrev.dir_entry;
-    //let file = File::open( PathWithString[i as usize].path_string );
-
-    println!("\n'{}'\n",  path_string);
-
-    let file = File::open(path_string).unwrap(); 
-    let audio_source = rodio::Decoder::new(BufReader::new(file)).unwrap();
-    sink.append(audio_source);
-
-    sink.sleep_until_end(); // play everything in queu
-    println!("done!");
 
 } // main
 
 
 fn highlight_nth(index: i32, path_w_string_vec: &Vec<PathWithString>, window: WINDOW) {
-    for i in 0..5 {
+    let maxlen = path_w_string_vec.len() as i32; // max list length
+    for i in 0..maxlen {
         if i == index {
             attr_on(A_STANDOUT() as u32);
         } else {
             attr_off(A_STANDOUT() as u32);
         }
         let text = &path_w_string_vec[i as usize].path_string;
-        mvprintw((i as i32) + 1, 2, text);
+        mvprintw((i as i32) + 2, 3, text);
     }
     wrefresh(window);
 }
