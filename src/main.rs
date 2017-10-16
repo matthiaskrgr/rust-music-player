8fd09210257cae0156ee2c5af3c92c25b2b339f8
Matthias Krüger <matthias.krueger@famsik.de>
2017-10-16 15:16:14 +0200

    better approach

--- src/main.rs
+++ src/main.rs
@@ -8,32 +8,43 @@ extern crate ncurses; // https://github.com/jeaye/ncurses-rs
 use std::fs;
 use std::fs::File;
 use std::io::BufReader;
-//use std::path::Path;
+use std::path::Path;
 use std::ffi::OsStr;
-
+use std::{time, thread};
 use ncurses::*;
 
 
-fn main() {
+struct PathWithString {
+    dirEntry: std::fs::DirEntry,
+    pathStr: String,
+}
 
-    /*
+fn main() {
     let mut playable_files = Vec::new();
     // iterate over current directory
     if let Ok(files) = fs::read_dir("./") {
         for filename in files {
             if let Ok(filename) = filename {
-                // filter playable files here
 
+                // filter playable files here
                 if filename.path().extension() == Some(OsStr::new("ogg")) {
                     //println!("Found file: {}", filename.path().display());
                     // collect filenames
-                    playable_files.push(filename);
+                    let path = format!("{:?}", filename.path());
+                    let PwS = PathWithString {
+                        dirEntry: filename,
+                        pathStr: path,
+                    };
+                    playable_files.push(PwS);
+
                 } // is ogg?
             }
         } // for filename in files
     } // all files in cwd
 
-
+    for i in &playable_files {
+        println!("{}", i.pathStr);
+    }
 
     // init ncurses
     initscr();
@@ -44,30 +55,32 @@ fn main() {
     // new window
     let w = newwin(10, 12, 1, 1);
     box_(w, 0, 0);
-
-    for i in 0..5 {
-        if i == 4 {
-            // highlighting
+    let i = 0;
+    for file in &playable_files {
+        // highlight
+        if i == 3 {
             wattron(w, A_STANDOUT as u32);
         } else {
             wattroff(w, A_STANDOUT as u32);
         }
-        mvprintw(i+1, 2, "menu entry\n");
+        mvprintw(i + 1, 2, "menu entry\n");
         wrefresh(w);
-    }
-
+    } // for
     wrefresh(w);
+
 
     printw("Testing ncurses screen, e to exit\n\n");
 
-    // display files in ncurses
-    for filename in &playable_files {
-        //let  songentry = &song.path();
-        addstr(&format!("Found file: {:?} \n", filename.path()));
+    for file in &playable_files {
+        addstr(&format!("{}\n", file.pathStr));
     }
 
     mvprintw(LINES() - 1, 0, "e to exit");
     refresh();
+
+
+
+    //thread::sleep(time::Duration::from_secs(4));
 
     let mut max_x = 0;
     let mut max_y = 0;
@@ -79,57 +92,13 @@ fn main() {
         max_y
     ));
 
-    let mut ch = getch();
-    // get user input (char), until we get an "e"
-    while ch != 'e' as i32 {
-        ch = getch();
-    }
-    endwin(); // close the ncurses window
+     highlightNth(0, &playable_files, w);
+let mut i = 0 as i32;
+let mut play ="";
+loop {
 
-
-    // play audio
-    println!("starting audio");
-    let endpoint = rodio::get_default_endpoint().unwrap(); // use default_endpoint() once this works
-    let sink = rodio::Sink::new(&endpoint);
-
-    // add all our found files to the queue
-    for filename in &playable_files {
-        // @TODO this will probably blow up if we have a lot of large files
-        let file = File::open(filename.path()).unwrap();
-        let audio_source = rodio::Decoder::new(BufReader::new(file)).unwrap();
-        sink.append(audio_source);
-    }
-    sink.sleep_until_end(); // play everything until end
-
-    println!("done");
-
-*/
-
-    let mut menus = Vec::new();
-    menus.push("menu one");
-    menus.push("menu two");
-    menus.push("menu three");
-    menus.push("menu four");
-    menus.push("menu five");
-    let menus = menus;
-    initscr();
-    //    raw();
-    let w = newwin(10, 12, 1, 1);
-    box_(w, 0, 0);
-
-    highlightNth(0, &menus, w);
-    wrefresh(w);
-
-    noecho(); // dont print typed stuff on screen
-    keypad(w, true); // enable keyboard input
-
-
-    let mut i = 0 as i32;
-    let mut play = "";
-    loop {
-
-        let mut ch = getch();
-        //print!("{}\n", ch as i32);
+       let mut ch = getch();
+               //print!("{}\n", ch as i32);
         if ch == 'e' as i32 {
             // terminate
             endwin();
@@ -139,17 +108,17 @@ fn main() {
                 continue;
             }
             i -= 1;
-            highlightNth(i, &menus, w);
+            highlightNth(i, &playable_files, w);
             wrefresh(w);
         } else if ch == 's' as i32 {
-            if i == (menus.len() - 1) as i32 {
+            if i == (playable_files.len() - 1) as i32 {
                 continue;
             }
             i += 1;
-            highlightNth(i, &menus, w);
+            highlightNth(i, &playable_files, w);
             wrefresh(w);
         } else if ch == 'p' as i32 {// play
-            play = menus[i as usize];
+            play = &playable_files[i as usize].pathStr;
             endwin();
             break;
         } else {
@@ -158,17 +127,34 @@ fn main() {
         wrefresh(w);
     }
     println!("playing  {}", play);
+    let index = i;
+    println!("playing audio");
+    let endpoint = rodio::get_default_endpoint().unwrap(); // use default_endpoint() once this works
+    let sink = rodio::Sink::new(&endpoint);
 
-}
+    let pathrev = &playable_files[i as usize];
+    let pathstr = &pathrev.pathStr;
+    let pathref = &pathrev.dirEntry;
+    //let file = File::open( PathWithString[i as usize].pathStr );
 
-fn highlightNth(index: i32, textVec: &Vec<&str>, window: WINDOW) {
+let file = File::open(pathstr).unwrap();
+let audio_source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+sink.append(audio_source);
+
+sink.sleep_until_end(); // play everything in queu
+println!("done!");
+
+} // main
+
+
+fn highlightNth(index: i32, pathWstringVec: &Vec<PathWithString>, window: WINDOW) {
     for i in 0..5 {
         if i == index {
             attr_on(A_STANDOUT() as u32);
         } else {
             attr_off(A_STANDOUT() as u32);
         }
-        let text = &textVec[i as usize];
+        let text = &pathWstringVec[i as usize].pathStr;
         mvprintw((i as i32) + 1, 2, text);
     }
     wrefresh(window);
